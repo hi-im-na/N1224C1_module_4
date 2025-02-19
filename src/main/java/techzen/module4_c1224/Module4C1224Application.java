28a0de17abc55241a78626032d05ec0b964ed362
hi-im-na <lengocanh2k@gmail.com>
2025-02-19 13:17:16 +0700

    init project

---
+++ src/main/java/techzen/module4_c1224/Module4C1224Application.java
@@ -0,0 +1,13 @@
+package techzen.module4_c1224;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Module4C1224Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Module4C1224Application.class, args);
+    }
+
+}
---
+++ src/test/java/techzen/module4_c1224/Module4C1224ApplicationTests.java
@@ -0,0 +1,13 @@
+package techzen.module4_c1224;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Module4C1224ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
